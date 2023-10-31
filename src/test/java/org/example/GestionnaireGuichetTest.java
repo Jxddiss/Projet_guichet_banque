2d55d36a153e1120c977da5d7e3894bfad7ff2cb
Nicholson <nrainvillejacques@dti.crosemont.quebec>
2023-10-31 15:29:35 -0400

    création du squelette du fichier test du GestionnaireGuichet

---
+++ src/test/java/org/example/GestionnaireGuichetTest.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GestionnaireGuichetTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void validerUtilisateur() {
+    }
+
+    @Test
+    void retraitCheque() {
+    }
+
+    @Test
+    void retraitEpargne() {
+    }
+
+    @Test
+    void depotCheque() {
+    }
+
+    @Test
+    void depotEpargne() {
+    }
+
+    @Test
+    void paiementFacture() {
+    }
+
+    @Test
+    void transfertFond() {
+    }
+
+    @Test
+    void afficherSoldeCompte() {
+    }
+
+    @Test
+    void creerClient() {
+    }
+
+    @Test
+    void creerCompte() {
+    }
+}
