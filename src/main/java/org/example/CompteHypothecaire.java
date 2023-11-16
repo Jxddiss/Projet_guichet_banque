98f4744b7470a0a41b41c819857a9c4c2b8601ed
Nicholson <nrainvillejacques@dti.crosemont.quebec>
2023-11-16 18:04:38 -0500

    debut migration

--- src/main/java/org/example/CompteHypothecaire.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-class CompteHypothecaire extends Compte {
-
-    public CompteHypothecaire(int numeroCompte,int codeClient, double montantTransfertMaximum) {
-        super(numeroCompte,codeClient,  montantTransfertMaximum);
-        this.type = "hypotheque";
-    }
-}
