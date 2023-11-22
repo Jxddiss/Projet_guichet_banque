0a1d20244288a04377b300c21806f1ffe7180a1b
Nicholson <nrainvillejacques@dti.crosemont.quebec>
2023-11-22 18:57:16 -0500

    travaille sur la classe Demande.java

--- src/main/java/org/example/projet_guichet_banque/model/Demande.java
+++ src/main/java/org/example/projet_guichet_banque/model/Demande.java
@@ -1,6 +1,46 @@
 package org.example.projet_guichet_banque.model;
 
 public class Demande {
-    public static String numeroDemande;
+    public static int compteurNumeroDemande = 0;
+    private int numeroDemande;
+    private int codeClient;
+    private String typeCompte;
+    private String statut;
 
+    public Demande(int codeClient, String typeCompte, String statut) {
+        compteurNumeroDemande += 1;
+        this.numeroDemande = compteurNumeroDemande;
+        this.codeClient = codeClient;
+        this.typeCompte = typeCompte;
+        this.statut = statut;
+    }
+
+    public int getNumeroDemande() {
+        return numeroDemande;
+    }
+
+    public int getCodeClient() {
+        return codeClient;
+    }
+
+    public String getTypeCompte() {
+        return typeCompte;
+    }
+
+    public String getStatut() {
+        return statut;
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
+
+    @Override
+    public String toString() {
+        return "Demande{" +
+                "codeClient=" + codeClient +
+                ", typeCompte='" + typeCompte + '\'' +
+                ", statut='" + statut + '\'' +
+                '}';
+    }
 }
