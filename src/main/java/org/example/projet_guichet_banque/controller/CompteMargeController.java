be8e3fd2a06979033b5990fdb54ab6ed616bd6cf
Nicholson <nrainvillejacques@dti.crosemont.quebec>
2023-12-03 23:19:56 -0500

    Enlever du code qui se répètent en créeant une classe parent

--- src/main/java/org/example/projet_guichet_banque/controller/CompteMargeController.java
+++ src/main/java/org/example/projet_guichet_banque/controller/CompteMargeController.java
@@ -21,57 +21,6 @@ import org.example.projet_guichet_banque.model.Transaction;
 import java.io.IOException;
 import java.util.Objects;
 
-public class CompteMargeController {
-    @FXML
-    private Label prenomUtilisateurLbl;
-    @FXML
-    private Label numCompteLbl;
-    @FXML
-    private Label soldeLbl;
-    @FXML
-    private Button quitterBtn;
-    @FXML
-    private TableView<Transaction> transactionTable;
-    @FXML
-    private TableColumn<Transaction, String> montantColonne;
-    @FXML
-    private TableColumn<Transaction, String> destinationColonne;
-    @FXML
-    private TableColumn<Transaction, String> typeColonne;
-    private ObservableList<Transaction> transactions;
-    private Scene scene;
-    private Parent root;
+public class CompteMargeController extends CompteControllerParent{
 
-    @FXML
-    public void initialize(){
-        String prenom = LoginController.gestionnaireGuichet.getClient().getPrenom();
-        prenom = prenom.substring(0, 1).toUpperCase() + prenom.substring(1);
-        prenomUtilisateurLbl.setText("Bonjour, " + prenom);
-        numCompteLbl.setText(String.format("%04d",AffichageCompteController.compteChoisi.getNumeroCompte()));
-        soldeLbl.setText(String.format("%.2f $",AffichageCompteController.compteChoisi.getSoldeCompte()));
-
-
-        transactions = FXCollections.observableArrayList(LoginController.gestionnaireGuichet.getTransactions(AffichageCompteController.compteChoisi.getNumeroCompte()));
-        montantColonne.setCellValueFactory(cellData -> {
-            String montantFormater = String.format("%.2f $",cellData.getValue().getMontant());
-            return new SimpleStringProperty(montantFormater);
-        });
-        destinationColonne.setCellValueFactory(cellData -> {
-            String compteFormater = "Compte : "+ String.format("%04d",cellData.getValue().getCompte());
-            return new SimpleStringProperty(compteFormater);
-        });
-        typeColonne.setCellValueFactory(new PropertyValueFactory<>("type"));
-        transactionTable.setItems(transactions);
-        transactionTable.setSelectionModel(null);
-    }
-
-    @FXML
-    public void quitterClick(ActionEvent actionEvent) throws IOException {
-        if (actionEvent.getSource() == quitterBtn){
-            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/vues/affichageCompte.fxml")));
-            scene = quitterBtn.getScene();
-            scene.setRoot(root);
-            ((Stage)scene.getWindow()).setTitle("Comptes");
-        }
-    }
 }
