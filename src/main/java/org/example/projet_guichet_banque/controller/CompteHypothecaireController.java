9d02ab115d3b15f39327148d6ef0272dc4d344c4
Nicholson <nrainvillejacques@dti.crosemont.quebec>
2023-12-03 23:22:02 -0500

    Enlever des import non utilisés

--- src/main/java/org/example/projet_guichet_banque/controller/CompteHypothecaireController.java
+++ src/main/java/org/example/projet_guichet_banque/controller/CompteHypothecaireController.java
@@ -1,25 +1,5 @@
 package org.example.projet_guichet_banque.controller;
 
-import javafx.application.Application;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Stage;
-import org.example.projet_guichet_banque.model.Compte;
-import org.example.projet_guichet_banque.model.Transaction;
-
-import java.io.IOException;
-import java.util.Objects;
 
 public class CompteHypothecaireController extends CompteControllerParent{
 
