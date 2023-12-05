826570111cbd765c111e993e1ff850cac67e37fe
Jamil <jfayad@dti.crosemont.qc.ca>
2023-12-04 23:58:50 -0500

    Ajout d'une horloge dans la scene princiale de login

--- src/main/java/org/example/projet_guichet_banque/controller/LoginController.java
+++ src/main/java/org/example/projet_guichet_banque/controller/LoginController.java
@@ -1,20 +1,25 @@
 package org.example.projet_guichet_banque.controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.example.projet_guichet_banque.model.GestionnaireGuichet;
 import org.example.projet_guichet_banque.model.GestionnaireGuichetDAO;
 import org.example.projet_guichet_banque.model.Setup;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
 
 public class LoginController {
@@ -25,11 +30,22 @@ public class LoginController {
     private PasswordField nipTxtField;
     @FXML
     private Button connecterBtn;
+    @FXML
+    private Label temps;
 
     @FXML
     public void initialize(){
         Setup.setup();
         gestionnaireGuichet = GestionnaireGuichetDAO.get();
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+            String formattedTime = dateFormat.format(new Date());
+            temps.setText(formattedTime);
+        }));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+
     }
 
     public void connecterClick(ActionEvent actionEvent) throws IOException {
@@ -69,4 +85,5 @@ public class LoginController {
             alert.show();
         }
     }
+
 }
