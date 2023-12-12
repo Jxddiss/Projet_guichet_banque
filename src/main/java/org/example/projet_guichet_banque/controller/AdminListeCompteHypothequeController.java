967753760fb41f231ff81afe4aef4983d7d72e48
Nicholson <nrainvillejacques@dti.crosemont.quebec>
2023-12-12 17:38:57 -0500

    fonction prélèvement hypothèque fait

--- src/main/java/org/example/projet_guichet_banque/controller/AdminListeCompteHypothequeController.java
+++ src/main/java/org/example/projet_guichet_banque/controller/AdminListeCompteHypothequeController.java
@@ -8,8 +8,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.example.projet_guichet_banque.model.Compte;
 import org.example.projet_guichet_banque.model.Transaction;
@@ -19,6 +21,8 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class AdminListeCompteHypothequeController extends AdminListeCompteParent{
+    @FXML
+    private TextField montantTxtField;
     @FXML
     @Override
     public void initialize(){
@@ -32,5 +36,31 @@ public class AdminListeCompteHypothequeController extends AdminListeCompteParent
 
         transactionsSansfiltre = LoginController.gestionnaireGuichet.getTransactionsAdmin();
         updateTransactions("hypotheque");
+    }
+
+    @FXML
+    public void preleverClick(){
+        try{
+            double montant = Double.parseDouble(montantTxtField.getText());
+            double solde = LoginController.gestionnaireGuichet.preleverMontant(tabCompte.getSelectionModel().getSelectedItem().getNumeroCompte(),montant);
+            if (solde == -2){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Prélèvement Impossible");
+                alert.show();
+            }else if(solde == -1){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Vous n'êtes pas autorisé à faire cette action");
+                alert.show();
+            }else{
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText("Prélèvement confirmé " +
+                        "\nTOTAL : "+ String.format("%.2f",solde));
+                alert.show();
+            }
+        }catch (Exception e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Veuiller entré un montant valide!");
+            alert.show();
+        }
     }
 }
