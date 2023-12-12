03d941a73e4cd38e4c9d9fc52437b2762b54214b
Nicholson <nrainvillejacques@dti.crosemont.quebec>
2023-12-12 11:56:20 -0500

    Travaille sur la fonction liste des comptes admin

--- src/main/java/org/example/projet_guichet_banque/controller/AdminListeCompteChequeController.java
+++ src/main/java/org/example/projet_guichet_banque/controller/AdminListeCompteChequeController.java
@@ -1,8 +1,40 @@
 package org.example.projet_guichet_banque.controller;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class AdminListeCompteChequeController {
+import java.io.IOException;
+import java.util.Objects;
 
+public class AdminListeCompteChequeController {
+    @FXML
+    private Label prenomUtilisateurLbl;
+    @FXML
+    private Button quitterBtn;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    public void initialize(){
+        String prenom = LoginController.gestionnaireGuichet.getClient().getPrenom();
+        prenom = prenom.substring(0, 1).toUpperCase() + prenom.substring(1);
+        prenomUtilisateurLbl.setText("Bonjour, " + prenom);
+
+    }
+    @FXML
+    public void quitterClick(ActionEvent actionEvent) throws IOException {
+        if (actionEvent.getSource() == quitterBtn){
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/vues/adminListeCompte.fxml")));
+            scene = quitterBtn.getScene();
+            scene.setRoot(root);
+            ((Stage)scene.getWindow()).setTitle("Liste de Comptes");
+        }
+    }
 }
--- src/main/java/org/example/projet_guichet_banque/controller/AdminListeCompteController.java
+++ src/main/java/org/example/projet_guichet_banque/controller/AdminListeCompteController.java
@@ -8,6 +8,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,8 +20,17 @@ public class AdminListeCompteController {
     private Label prenomUtilisateurLbl;
     @FXML
     private Button quitterBtn;
+    @FXML
+    private Pane chequePaneBtn;
+    @FXML
+    private Pane epargnePaneBtn;
+    @FXML
+    private Pane hypothecairePaneBtn;
+    @FXML
+    private Pane margePaneBtn;
     private Scene scene;
     private Parent root;
+
 
     @FXML
     public void initialize(){
@@ -27,6 +38,37 @@ public class AdminListeCompteController {
         prenom = prenom.substring(0, 1).toUpperCase() + prenom.substring(1);
         prenomUtilisateurLbl.setText("Bonjour, " + prenom);
 
+    }
+
+    @FXML
+    public void ouvrirClick(MouseEvent mouseEvent)throws IOException{
+        if (mouseEvent.getSource() == chequePaneBtn){
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/vues/adminListeCompteCheque.fxml")));
+            scene = chequePaneBtn.getScene();
+            scene.setRoot(root);
+            ((Stage)scene.getWindow()).setTitle("Liste Compte Chèque");
+        }
+
+        if (mouseEvent.getSource() == epargnePaneBtn){
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/vues/adminListeCompteEpargne.fxml")));
+            scene = epargnePaneBtn.getScene();
+            scene.setRoot(root);
+            ((Stage)scene.getWindow()).setTitle("Liste Compte Épargne");
+        }
+
+        if (mouseEvent.getSource() == hypothecairePaneBtn){
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/vues/adminListeCompteHypotheque.fxml")));
+            scene = hypothecairePaneBtn.getScene();
+            scene.setRoot(root);
+            ((Stage)scene.getWindow()).setTitle("Liste Compte Hypothécaire");
+        }
+
+        if (mouseEvent.getSource() == margePaneBtn){
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/vues/adminListeCompteMarge.fxml")));
+            scene = margePaneBtn.getScene();
+            scene.setRoot(root);
+            ((Stage)scene.getWindow()).setTitle("Liste Marges de Crédits");
+        }
     }
 
     @FXML
