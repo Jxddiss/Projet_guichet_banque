ed8ef830bbb13935f17445070e09cdf212d88954
Nicholson <nrainvillejacques@dti.crosemont.quebec>
2023-12-12 12:12:39 -0500

    Travaille sur la fonction liste des comptes admin

--- src/main/java/org/example/projet_guichet_banque/controller/AdminListeCompteHypothequeController.java
+++ src/main/java/org/example/projet_guichet_banque/controller/AdminListeCompteHypothequeController.java
@@ -13,28 +13,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class AdminListeCompteHypothequeController {
-    @FXML
-    private Label prenomUtilisateurLbl;
-    @FXML
-    private Button quitterBtn;
-    private Scene scene;
-    private Parent root;
+public class AdminListeCompteHypothequeController extends AdminListeCompteParent{
 
-    @FXML
-    public void initialize(){
-        String prenom = LoginController.gestionnaireGuichet.getClient().getPrenom();
-        prenom = prenom.substring(0, 1).toUpperCase() + prenom.substring(1);
-        prenomUtilisateurLbl.setText("Bonjour, " + prenom);
-
-    }
-    @FXML
-    public void quitterClick(ActionEvent actionEvent) throws IOException {
-        if (actionEvent.getSource() == quitterBtn){
-            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/vues/adminListeCompte.fxml")));
-            scene = quitterBtn.getScene();
-            scene.setRoot(root);
-            ((Stage)scene.getWindow()).setTitle("Liste de Comptes");
-        }
-    }
 }
