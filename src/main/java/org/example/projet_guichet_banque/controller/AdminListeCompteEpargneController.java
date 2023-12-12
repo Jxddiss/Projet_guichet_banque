175f3c46b321dceac5a83f34dfbd25c80d1ae0b2
Nicholson <nrainvillejacques@dti.crosemont.quebec>
2023-12-12 13:36:40 -0500

    Travaille sur la fonction liste des comptes admin

--- src/main/java/org/example/projet_guichet_banque/controller/AdminListeCompteEpargneController.java
+++ src/main/java/org/example/projet_guichet_banque/controller/AdminListeCompteEpargneController.java
@@ -1,6 +1,8 @@
 package org.example.projet_guichet_banque.controller;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,10 +11,21 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import org.example.projet_guichet_banque.model.Compte;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class AdminListeCompteEpargneController extends AdminListeCompteParent{
-
+    @FXML
+    @Override
+    public void initialize(){
+        super.initialize();
+        ArrayList<Compte> comptes;
+        ObservableList<Compte> comptesObs;
+        comptes = LoginController.gestionnaireGuichet.getComptesEpargnes();
+        comptesObs = FXCollections.observableArrayList(comptes);
+        tabCompte.setItems(comptesObs);
+    }
 }
